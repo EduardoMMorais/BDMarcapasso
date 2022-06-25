73761edf25568e79230bf9ba2420aa9a9e55ca2a
Eduardo Yada <eduardo.yada@usp.br>
2022-06-25 16:15:07 -0300

    Add distribution shift plots

--- EDA/0-generator.R
+++ EDA/0-generator.R
@@ -9,7 +9,7 @@ columns_list <- yaml.load_file("./auxiliar/columns_list.yaml")
 for(outcome_column in columns_list$outcome_columns){
   dir.create(file.path(paste0('./results/', outcome_column)),
              showWarnings = FALSE)
-  
+
   rmarkdown::render('2-tables.Rmd',
                     params = list(outcome_column = outcome_column),
                     output_file = paste0('./results/', outcome_column, '/2-tables.pdf'),
@@ -19,9 +19,13 @@ for(outcome_column in columns_list$outcome_columns){
                     params = list(outcome_column = outcome_column),
                     output_file = paste0('./results/', outcome_column, '/3-plots.pdf'),
                     clean=TRUE)
-  
+
   rmarkdown::render('4-correlations.Rmd',
                     params = list(outcome_column = outcome_column),
                     output_file = paste0('./results/', outcome_column, '/4-correlations.pdf'),
                     clean=TRUE)
 }
+
+rmarkdown::render('5-distribution_shift.Rmd',
+                  output_file = './results/5-distribution_shift.pdf',
+                  clean=TRUE)
