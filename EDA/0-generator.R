3470bb34c4177d084c2b4f76fc75435b90c993d6
Eduardo Yada <eduardo.yada@usp.br>
2022-07-21 18:58:02 -0300

    Filter selected features for modeling

--- EDA/0-generator.R
+++ EDA/0-generator.R
@@ -2,42 +2,73 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 rmarkdown::render('1-processing.Rmd',
                   output_file = './results/1-processing.pdf',
-                  clean=TRUE)
+                  clean = TRUE)
 
 columns_list <- yaml.load_file("./auxiliar/columns_list.yaml")
 
-outcome_columns = setdiff(columns_list$outcome_columns, 
-                          c('death_intraop', 'death_hospitalar', 'death_readmission', 'death'))
+outcome_columns = setdiff(
+  columns_list$outcome_columns,
+  c(
+    'death_intraop',
+    'death_hospitalar',
+    'death_readmission',
+    'death'
+  )
+)
 
-for(outcome_column in outcome_columns){
+for (outcome_column in outcome_columns[1]) {
   dir.create(file.path(paste0('./results/', outcome_column)),
              showWarnings = FALSE)
-
-  rmarkdown::render('2-tables.Rmd',
-                    params = list(outcome_column = outcome_column),
-                    output_file = paste0('./results/', outcome_column, '/2-tables.pdf'),
-                    clean=TRUE)
-
-  rmarkdown::render('3-plots.Rmd',
-                    params = list(outcome_column = outcome_column),
-                    output_file = paste0('./results/', outcome_column, '/3-plots.pdf'),
-                    clean=TRUE)
-
-  rmarkdown::render('4-correlations.Rmd',
-                    params = list(outcome_column = outcome_column),
-                    output_file = paste0('./results/', outcome_column, '/4-correlations.pdf'),
-                    clean=TRUE)
   
+  rmarkdown::render(
+    '2-tables.Rmd',
+    params = list(outcome_column = outcome_column),
+    output_file = paste0('./results/', outcome_column, '/2-tables.pdf'),
+    clean = TRUE
+  )
   
-  rmarkdown::render('6-model.Rmd',
-                    params = list(outcome_column = outcome_column),
-                    output_file = paste0('./results/', outcome_column, '/6-model.pdf'),
-                    clean=TRUE)
+  rmarkdown::render(
+    '3-plots.Rmd',
+    params = list(outcome_column = outcome_column),
+    output_file = paste0('./results/', outcome_column, '/3-plots.pdf'),
+    clean = TRUE
+  )
+  
+  rmarkdown::render(
+    '4-correlations.Rmd',
+    params = list(outcome_column = outcome_column),
+    output_file = paste0('./results/', outcome_column, '/4-correlations.pdf'),
+    clean = TRUE
+  )
+  
+  cat_features_list = readRDS(
+    sprintf(
+      "../EDA/auxiliar/significant_columns/categorical_%s.rds",
+      outcome_column
+    )
+  )
+  
+  num_features_list = readRDS(
+    sprintf(
+      "../EDA/auxiliar/significant_columns/numerical_%s.rds",
+      outcome_column
+    )
+  )
+  
+  features_list = c(cat_features_list, num_features_list)
+  
+  rmarkdown::render(
+    '6-model.Rmd',
+    params = list(outcome_column = outcome_column,
+                  features_list = features_list),
+    output_file = paste0('./results/', outcome_column, '/6-model.pdf'),
+    clean = TRUE
+  )
 }
 
 rmarkdown::render('5-distribution_shift.Rmd',
                   output_file = './results/5-distribution_shift.pdf',
-                  clean=TRUE)
+                  clean = TRUE)
 
 rmarkdown::render('7-final_results.Rmd',
                   output_file = './results/7-final_results.pdf',
